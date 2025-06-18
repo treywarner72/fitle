9ffa7a59c2186a51bc616f8a64688f352f2ddf06
Trey Warner <trey.warner@hey.com>
2025-06-18 10:31:38 +0200

    remove ipynbcheckpoints

--- .ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from .param import *
-from .model import *
-from .fitting import *
-from .cost import *
-from .pdfs import *
-from .compiler import *
-
-__all__ = ["Param", "P", "Q", "U", "X", "index", "I", "Model","Reduction","const","indecise","fit","FitResult","MSE","NLL","Chi2", "BinnedNLL","gaussian", "exponential", "uniform", "crystal_ball", "Compiler"]
